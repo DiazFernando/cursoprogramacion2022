50698eea24467f0f274f96c7ffb245138284d18a
DiazFernando <104173655+DiazFernando@users.noreply.github.com>
2022-04-24 23:55:06 -0300

    ejercicios

---
+++ src/calcularPorcentaje.ts
@@ -0,0 +1,17 @@
+//variable para el precio del producto
+let precioProducto : number = Number(prompt("ingrese el precio"));
+
+//variable para el porcentaje del descuento
+let porcentajeDescuento : number = 0.1;
+
+//calculo del descuento
+let descuento : number = precioProducto*porcentajeDescuento;
+
+//calculo para el precio final
+let precioFinal : number = precioProducto-descuento;
+
+
+//Precio final mostrado en consola
+console.log("el precio final es: " + precioFinal);
+
+
---
+++ src/ejercicioprompt.ts
@@ -0,0 +1,5 @@
+let numero1: number = Number(prompt("ingrese un numero"));
+let numero2: number = Number(prompt("ingrese un numero"));
+let resultado: number = numero1 + numero2;
+
+console.log("el resultado es: " resultado);
