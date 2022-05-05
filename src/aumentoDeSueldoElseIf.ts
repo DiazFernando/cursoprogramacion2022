967d4408592ebba279ce5ebcc377f65d7010ce73
DiazFernando <104173655+DiazFernando@users.noreply.github.com>
2022-05-05 00:36:49 -0300

    ejercicio a entregar else if se aplico comentarios

--- src/aumentoDeSueldoElseIf.ts
+++ src/aumentoDeSueldoElseIf.ts
@@ -1,22 +1,25 @@
+//variable para ingresar el sueldo
 let sueldoActual: number = Number(prompt("ingrese su sueldo"));
+//variable para el sueldo con aumento
 let sueldoConAumento: number = 0;
 
+// estructura de selección para saber que porcentaje de aumento le corresponde segun su sueldo
 if (sueldoActual < 15000) {
-  sueldoConAumento = sueldoActual * 1.2;
+  sueldoConAumento = sueldoActual * 1.2; //obtiene un 20% de aumento
   console.log(
     "obtiene un 20% de aumento, su sueldo será de " + sueldoConAumento
   );
 } else if (sueldoActual > 15000 && sueldoActual < 20000) {
-  sueldoConAumento = sueldoActual * 1.1;
+  sueldoConAumento = sueldoActual * 1.1; //obtiene un 10% de aumento
   console.log(
     "obtiene un 10% de aumento, su sueldo será de " + sueldoConAumento
   );
 } else if (sueldoActual > 20000 && sueldoActual < 25000) {
-  sueldoConAumento = sueldoActual * 1.05;
+  sueldoConAumento = sueldoActual * 1.05; //obtiene un 5% de aumento
   console.log(
     "obtiene un 5% de aumento, su sueldo será de " + sueldoConAumento
   );
 } else {
-  sueldoConAumento = sueldoActual;
+  sueldoConAumento = sueldoActual; //no obtiene aumento
   console.log("esta vez no obtiene aumento ");
 }
