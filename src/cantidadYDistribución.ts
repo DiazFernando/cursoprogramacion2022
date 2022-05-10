bb0a367ea206a6e1364f566a7953cfb06bf4db5c
DiazFernando <104173655+DiazFernando@users.noreply.github.com>
2022-05-09 21:46:06 -0300

    correccion de ejercicio se corrigió el nombre de la variable de línea 2, para que quede mas representativa

--- src/cantidadYDistribución.ts
+++ src/cantidadYDistribución.ts
@@ -1,18 +1,18 @@
 let valorIngresado: number = Number(prompt("ingrese un valor")); // variable para ingresar un numero entero
-let valoresMáximos: number = 0;  // variable para cantidad de valores máximos ingresados
+let cantidadMáximos: number = 0;  // variable para cantidad de valores máximos ingresados
 let valoresTotales: number = 0;  // variable para la cantidad de valores ingresados (cualquier numero entero)
 let porcentajeDeMáximos: number = 0;  //Variable para el porcentaje de valores maximos ingresados sobre el total
 
 // mientras el valor ingresado sea distinto a 0 inciar el bucle
 while (valorIngresado !== 0) {
   if (valorIngresado > 0) { //si el valor ingresado es mayor a 0
-    valoresMáximos++;  // suma 1 por cada vuelta en la variable de valores maximos
+    cantidadMáximos++;  // suma 1 por cada vuelta en la variable de cantidad maximos
   }
   valoresTotales++; // suma 1 por cada velta en la variable de valores totales
   valorIngresado = Number(prompt("ingrese un valor")); // si el valor ingresado es distinto a 0 volver a ingresar un numero entero
 }
 if (valoresTotales > 0) {  // si la variable valoresTotales es mayor a 0 realizar el calculo del porcentaje
-  porcentajeDeMáximos = (valoresMáximos * 100) / valoresTotales; // 
+  porcentajeDeMáximos = (cantidadMáximos * 100) / valoresTotales; // 
   console.log(
-    valoresMáximos + " valores positivos, " + porcentajeDeMáximos + "% del total de valores ingresados");
+    cantidadMáximos + " valores positivos, " + porcentajeDeMáximos + "% del total de valores ingresados");
 }
