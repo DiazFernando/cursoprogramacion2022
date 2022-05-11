4fb8fdfc7c0499c4c74148b884cf9de09900705a
DiazFernando <104173655+DiazFernando@users.noreply.github.com>
2022-05-10 22:09:11 -0300

    ejercicio potenciar

--- modulación/potencia.ts
+++ modulación/potencia.ts
@@ -1,9 +1,3 @@
-/*Realice un programa que devuelva la potencia de un número. 
-La base y el exponente deben ser ingresados por teclado. 
-Sólo deben admitirse exponentes mayores o iguales a cero. 
-Recuerde que el resultado de un numero elevado a 0 es 1.
-Separe la lógica de calcular la potencia utilizando métodos.*/
-
 function potenciar() {
   if (exponente === 0) {
     resultado = 1;
