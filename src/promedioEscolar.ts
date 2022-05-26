68ebeff0dc5f94d424034a38849779de02771ce8
DiazFernando <104173655+DiazFernando@users.noreply.github.com>
2022-05-25 21:16:30 -0300

    Ejercicio para entregar

---
+++ src/promedioEscolar.ts
@@ -0,0 +1,49 @@
+//Variable para ingresar la cantidad de alumnos
+let numeroDeAlumnos: number = Number(prompt("Ingrese cantidad de Alumnos"));
+//Arreglo que almacena el nombre de los alumnos ingresados por el usuario
+let nombresDelAlumnos: string[] = new Array(numeroDeAlumnos);
+//Arreglo que almacena las notas del primer trimestre
+let notasTrimestre1: number[] = new Array(numeroDeAlumnos);
+//Arreglo que almacena las notas del segundo trimestre
+let notasTrimestre2: number[] = new Array(numeroDeAlumnos);
+//Arreglo que almacena las notas del tercer trimestre
+let notasTrimestre3: number[] = new Array(numeroDeAlumnos);
+
+//Bucle para ingresar los datos ingresados por el usuario a los distintos arreglos
+for (let indice: number = 0; indice < numeroDeAlumnos; indice++) {
+  nombresDelAlumnos[indice] = prompt("Ingrese nombre del Alumno");
+  notasTrimestre1[indice] = Number(prompt("Ingrese nota del primer trimestre"));
+  notasTrimestre2[indice] = Number(
+    prompt("Ingrese nota del segundo trimestre")
+  );
+  notasTrimestre3[indice] = Number(prompt("Ingrese nota del tercer trimestre"));
+}
+
+//Variable para buscar un alumno ingresado por el usuario
+let alumnoBuscado: string = prompt(
+  "Ingrese el nombre del alumno que esta buscando"
+);
+//Variable para encontrar la posición del alumno buscado dentro del arreglo
+let posición: number = 0;
+//Variable para cortar el bucle una vez encontrado la posición del alumno buscado
+let encontrado: boolean = false;
+//Variable para calcular el promedio anual del alumno buscado
+let promedioAnual: number = 0;
+//Bucle para encontrar la posición del alumno buscado
+while (posición < numeroDeAlumnos && !encontrado) {
+  if (nombresDelAlumnos[posición] === alumnoBuscado) {
+    encontrado = true;
+    promedioAnual =
+      (notasTrimestre1[posición] +
+        notasTrimestre2[posición] +
+        notasTrimestre3[posición]) /
+      3;
+  }
+  posición++;
+}
+
+if (encontrado) {
+  console.log("El promedio anual de " + alumnoBuscado + " es " + promedioAnual);
+} else {
+  console.log("No se encontro al alumno " + alumnoBuscado);
+}
---
+++ src/promedioEscolarModularizado.ts
@@ -0,0 +1,51 @@
+//Función para cargar los datos por el usuario
+function cargarDatos (nombresAlumnos:string[],nota1:number[],nota2:number[],nota3:number[]):void {
+  for (let indice: number = 0; indice < nombresAlumnos.length; indice++) {
+    nombresAlumnos[indice] = prompt("Ingrese nombre del Alumno");
+    nota1[indice] = Number(prompt("Ingrese nota del primer trimestre"));
+    nota2[indice] = Number(prompt("Ingrese nota del segundo trimestre"));
+    nota3[indice] = Number(prompt("Ingrese nota del tercer trimestre"));
+  }
+}
+
+//Función para calcular el promedio anual del alumno buscado
+function calcularPromedio (nota1:number,nota2:number,nota3:number):number {
+  let promedio:number = (nota1+nota2+nota3)/3;
+  return promedio;
+}
+
+//Función para buscar un alumno
+function buscarAlumno (alumnoBuscado:string,nombresDeAlumnos:string[],numeroDeAlumnos:number):number {
+let indice: number = 0;
+let posiciónDelAlumno:number = 0; 
+while (indice < numeroDeAlumnos && posiciónDelAlumno === 0) {
+  if (nombresDeAlumnos[indice] === alumnoBuscado) {
+      posiciónDelAlumno=indice;
+    }
+    indice++ ;
+  }
+  return posiciónDelAlumno;
+}
+
+//Variable para ingresar la cantidad de alumnos
+let numeroDeAlumnos: number = Number(prompt("Ingrese cantidad de Alumnos"));
+//Arreglo que almacena el nombre de los alumnos ingresados por el usuario
+let nombresDeAlumnos: string[] = new Array(numeroDeAlumnos);
+//Arreglo que almacena las notas del primer trimestre
+let notasTrimestre1: number[] = new Array(numeroDeAlumnos);
+//Arreglo que almacena las notas del segundo trimestre
+let notasTrimestre2: number[] = new Array(numeroDeAlumnos);
+//Arreglo que almacena las notas del tercer trimestre
+let notasTrimestre3: number[] = new Array(numeroDeAlumnos);
+
+cargarDatos(nombresDeAlumnos,notasTrimestre1,notasTrimestre2,notasTrimestre3);
+//Variable para buscar un alumno ingresado por el usuario
+let alumnoBuscado: string = prompt("Ingrese el nombre del alumno que esta buscando");
+//variable para guardar la posición del alumno buscado dentro del arreglo nombres nombresDeAlumnos
+let indiceDelAlumno:number = buscarAlumno(alumnoBuscado,nombresDeAlumnos,numeroDeAlumnos);
+//condicion para imprimir por consola el promedio anual del alumno buscado
+if (indiceDelAlumno !== 0) {
+  console.log("El promedio anual de " + alumnoBuscado + " es " + calcularPromedio(notasTrimestre1[indiceDelAlumno],notasTrimestre2[indiceDelAlumno],notasTrimestre3[indiceDelAlumno]));
+} else {
+  console.log("No se encontro al alumno " + alumnoBuscado);
+}
